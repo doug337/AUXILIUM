3b591a3642a5498f0ae1b57735f2e64292517bb3
aluno <estelapaula15@bol.com.br>
2018-03-06 16:36:19 -0300

    AXESE1 no repositorio

---
+++ AXESE1/src/main/java/classes/Usuario.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+import java.util.Date;
+import javafx.scene.text.Text;
+
+/**
+ *
+ * @author aluno
+ */
+public class Usuario {
+    
+    String cod_usuario;
+    String email;
+    String senha;
+    Text cidade;
+    Text estado;
+    Date dat_nasc;
+    
+}
