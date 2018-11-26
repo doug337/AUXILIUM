d5ce483a9abe343e58bf3a2fce3b69e263826fbb
GabrielProgrammer <gabrielsilvaplay16@gmail.com>
2018-11-26 10:00:22 -0200

    Menu Dropdown

--- AUXILIUM/AUXILLIUM /EDIT.php
+++ AUXILIUM/AUXILLIUM /EDIT.php
@@ -13,24 +13,21 @@
 
 
 <!DOCTYPE html>
-
 <html>
-
     <head>
-
         <title> PHP UPDATE DATA </title>
 
-        <meta charset="UTF-8">
+        <meta charset="utf-8">
 
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="stylesheet" type="text/css" href="css/estilinho.css">
 
     </head>
 
     <body>
 
         <form action="EDITSERV.php" method="post">
-
-            ID To Update: <input type="text" name="username" required><br><br>
 
             Nome:<input type="text" name="nome" required><br><br>
 
@@ -39,6 +36,15 @@
             Email:<input type="text" name="email" required><br><br>
 
             <input type="submit" name="update" value="Update Data">
+
+            <div class="dropdown">
+              <button class="dropbtn">Usuários</button>
+              <div class="dropdown-content">
+                <a href="#"><?php echo $nome?></a>
+                <a href="#">Usuário 2</a>
+                <a href="#">Usuário 3</a>
+              </div>
+            </div>
 
         </form>
 
